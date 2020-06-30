ef5306227bf1a18c1f494c293084ec615d12fa7f
lucien <lamborkingly@gmail.com>
2020-06-29 23:22:05 -0400

    added first ui test

---
+++ src/test/calculator.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Calculator from "../component/calculator";
+
+import renderer from "react-test-renderer";
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Calculator />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders correctly", () => {
+  const Calc = renderer.create(<Calculator />).toJSON();
+  expect(Calc).toMatchSnapshot();
+});
